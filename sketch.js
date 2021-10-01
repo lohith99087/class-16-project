c186868f357690e49d6db2dfa76863023505a16c
lohith99087 <90755493+lohith99087@users.noreply.github.com>
2021-10-01 16:36:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+var box1;
+var box2;
+function setup() {
+  createCanvas(600, 400);
+box1= new Box ;
+box2= new Box ;
+}
+
+function draw() {
+  background(220);
+box1.show();
+box1.set_speed(1);
+box2.show();
+box2.set_width(100);
+}
+
